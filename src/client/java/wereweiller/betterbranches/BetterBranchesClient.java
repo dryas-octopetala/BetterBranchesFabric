822f8495fb6b0245945940bfb7f4f2f0a7425554
li-aven <avenjli@gmail.com>
2024-12-09 16:27:35 -0500

    Initial commit

---
+++ src/client/java/wereweiller/betterbranches/BetterBranchesClient.java
@@ -0,0 +1,10 @@
+package wereweiller.betterbranches;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterBranchesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/wereweiller/betterbranches/BetterBranchesDataGenerator.java
@@ -0,0 +1,11 @@
+package wereweiller.betterbranches;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterBranchesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
